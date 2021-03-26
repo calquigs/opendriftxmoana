5511c1dc75ac3b860d751b701819ad7146168779
Calvin Quigley <calvinquigley@Calvins-MBP-2.fritz.box>
2021-03-27 11:48:41 +1300

    defined functions to extract data from .nc, construct matrices, and plot points

--- netcdfprocessing_qgisbins.py
+++ netcdfprocessing_qgisbins.py
@@ -8,81 +8,68 @@ import matplotlib.pyplot as plt
 import math
 #from mpl_toolkits.basemap import Basemap
 from timeit import default_timer as timer
-
-print('parse input file')
-fn = os.path.expanduser("~/Desktop/14mussels_1day_output.nc")
-traj = nc.Dataset(fn)
-
-print('extract variables')
-lon = traj["lon"][:]
-lat = traj["lat"][:]
-status = traj["status"][:]
-time = traj["time"][:]
-
-print('extract final timestep of each particle from status variable')
-finaltimes = [np.where(part > 0)[-1] for part in status]
-finaltimes = [t.item() if len(t) else -1 for t in finaltimes]
-
-print('extract start lon, lat for each particle (first nonmasked value)')
-startlon = [part.compressed()[0] for part in lon]
-startlat = [part.compressed()[0] for part in lat]
-
-print('extract final lon,lat for each particle')
-finallon = []
-finallat = []
-count = 0
-for final in finaltimes:
-    #only include succesfully settled
-    #if final == (whatever number):
-    #clip spawning season
-    #finaldt = starttime + final * timestep - age
-    finallon.append(lon[count, final])
-    finallat.append(lat[count, final])
-    count += 1
-
-print('clip to startlon/lat of complete trajectories only (ideally all complete)')
-startlon2 = []
-startlat2 = []
-count = 0
-
-for i in finallon:
-    if math.isnan(finallon[count]) == False:
-        startlon2.append(startlon[count])
-    if math.isnan(finallat[count]) == False:
-        startlat2.append(startlat[count])
-    count += 1
-
-print('clip to finallat/lon of complete trajetories')
-finallon = [x for x in finallon if math.isnan(x) == False]
-finallat = [x for x in finallat if math.isnan(x) == False]
-
-print('converting lons/lats to points')
 import shapely
 from shapely.geometry import Point
 from shapely.geometry import shape
-startpoints = []
-count = 0
-for i in startlon2:
-    startpoints.append(Point(startlon2[count], startlat2[count]))
-    count += 1
-
-finalpoints = []
-count = 0
-for i in finallon:
-    finalpoints.append(Point(finallon[count], finallat[count]))
-    count += 1
-
-print('get bins from shapefile')
 import shapefile
-shape_filename = os.path.expanduser("~/Desktop/settlement_bins/settlement_bins.shp")
+import seaborn as sns
+import pandas as pd
+from mpl_toolkits.basemap import Basemap
+
+######################
+#extract data from .nc
+######################
+def nc_to_startfinal_points(nc_in):
+    '''
+    Extract the start and final locations as shapely Points and end status 
+    of each particle from an OpenDrift output .nc file.
+
+    nc_in: the path to the .nc file
+    '''
+    #open file+extract variables
+    traj = nc.Dataset(nc_in)
+    lon = traj["lon"][:]
+    lat = traj["lat"][:]
+    status = traj["status"][:]
+    
+    finaltimes = [np.where(part > 0)[-1] for part in status]
+    finaltimes = [t.item() if len(t) else -1 for t in finaltimes]
+    #extract start lon, lat for each particle (first nonmasked value)
+    startlon = [part.compressed()[0] for part in lon]
+    startlat = [part.compressed()[0] for part in lat]
+    #extract final lon,lat, and status for each particle
+    finallon = []
+    finallat = []
+    finalstatus = []
+    count = 0
+    for final in finaltimes:
+        finallon.append(lon[count, final])
+        finallat.append(lat[count, final])
+        finalstatus.append(status[count, final])
+        count += 1
+    #convert status flags to meanings
+    statusmeanings = traj.variables['status'].flag_meanings
+    statusmeanings = statusmeanings.split()
+    finalstatus = [statusmeanings[finalstatus[i]] for i in range(len(finalstatus))]
+    #convert start/final lon,lat to points
+    startpoints = []
+    for i in range(len(startlon)):
+        startpoints.append(Point(startlon[i], startlat[i]))
+    finalpoints = []
+    for i in range(len(finallon)):
+        finalpoints.append(Point(finallon[i], finallat[i]))
+    sfpoints = []
+    for i in range(len(startpoints)):
+        sfpoints.append([startpoints[i], finalpoints[i], finalstatus[i]])
+    return sfpoints
+
+########################################
+#read in settlement bins and create grid
+########################################
+shape_filename = os.path.expanduser("~/Desktop/rho_settlement_bins/rho_settlement_bins.shp")
 shp = shapefile.Reader(shape_filename)
 bins = shp.shapes()
 records = shp.records()
-
-# The settlement cells are distributed only on the coastlines.
-# To have O(1) lookups, we need to create a regular grid
-# that just contains the indices of the "real" settlement bin
-# that corresponds to the grid cell (if any).
 
 class Grid:
     def __init__(self, bins, records):
@@ -155,10 +142,9 @@ class Grid:
         plt.scatter(query_point.x, query_point.y, s=80, marker="*")
         plt.show()
 
-print('creating settlement-bin lookup grid...')
 grid = Grid(bins, records)
 
-print('checking if start/end points fall in bins')
+#checking if start/end points fall in bins
 missed_bin_count = 0
 startbins = []
 for i in range(len(startpoints)):
@@ -187,108 +173,133 @@ for i in range(len(startpoints)):
         sys.exit(1)
     startbins.append([i, records[start_bin_idx][0]])
 
-finalbins = []
-for i in range(len(finalpoints)):
-    final_bin_idx = grid.get_bin_idx(finalpoints[i].x, finalpoints[i].y)
-    if final_bin_idx < 0:
-        #print(f'point {i} did not end in a bin')
-        continue  # didn't end within a bin
-    finalbins.append([i, records[final_bin_idx][0]])
-            
-startfinalbins = []
-for i in range(len(finalpoints)):
-    start_bin_idx = grid.get_bin_idx(startpoints[i].x, startpoints[i].y)
-    final_bin_idx = grid.get_bin_idx(finalpoints[i].x, finalpoints[i].y)
-    startfinalbins.append([start_bin_idx, final_bin_idx])
-       
-#create empty connectivity matrix
-conmat = np.empty((len(bins), len(bins)+1))
+################
+#create matrices
+################
+def points_to_binmatrix(matrix, sfpoints):
+    '''
+    Build connectivity matrix from start+final points of trajectories using bin IDs.
 
-#fill matrix!
-for i in startfinalbins:
-    if i[0] > 0:
-        if i[1] == -1:
-            conmat[i[0], -1] += 1
-        else:
-            conmat[i[0], i[1]] += 1
+    matrix: a 2d np.array of size (n, n+1), where n is equal to len(bins).
+    sfpoints: a list of lists x where each element of x is a list that contains
+    the start and final locations as shapely Points and end status for each particle.
+    e.g. x = [[Point,Point, status],[Point, Point, status]]
+    '''
+    for x in sfpoints:
+        sfbins = []
+        for i in range(len(x)):
+            start_bin_idx = grid.get_bin_idx(x[i][0].x, x[i][0].y)
+            final_bin_idx = grid.get_bin_idx(x[i][1].x, x[i][1].y)
+            sfbins.append([start_bin_idx, final_bin_idx])
+        for i in sfbins:
+            if i[0] > 0:
+                if i[1] == -1:
+                    matrix[i[0], -1] += 1
+                else:
+                    matrix[i[0], i[1]] += 1
+    for i in range(len(matrix)-1):
+        if sum(matrix[i]) > 0:
+            matrix[i] = matrix[i]/sum(matrix[i])
+    matrix = matrix[:,:-1]
+    return matrix
 
-#convert to percent settlers
-conmatpercent = np.empty((len(bins), len(bins)+1))
-for i in range(len(conmat)-1):
-    if sum(conmat[i]) > 0:
-        conmatpercent[i] = conmat[i]/sum(conmat[i])
 
-conmatpercent = conmatpercent[:,:-1]
+def points_to_regionmatrix(matrix, sfpoints):
+    '''
+    Build connectivity matrix from start+final points of trajectories using settlement regions.
 
+    matrix: a 2d np.array of size (n, n+1), where n is equal to len(regions).
+    sfpoints: a list of lists x where each element of x is a list that contains
+    the start and final locations as shapely Points and end status for each particle.
+    e.g. x = [[Point,Point, status],[Point, Point, status]]
+    '''
+    for x in sfpoints:
+        sfregions = []
+        for i in range(len(x)):
+            start_bin_region = grid.get_bin_region(x[i][0].x, x[i][0].y)
+            final_bin_region = grid.get_bin_region(x[i][1].x, x[i][1].y)
+            sfregions.append([start_bin_region, final_bin_region])
+        for i in sfregions:
+            if i[0] > 0:
+                if i[1] == -1:
+                    matrix[i[0], -1] += 1
+                else:
+                    matrix[i[0], i[1]] += 1
+    for i in range(len(matrix)-1):
+        if sum(matrix[i]) > 0:
+            matrix[i] = matrix[i]/sum(matrix[i])
+    matrix = matrix[:,:-1]
+    return matrix
+
+
+#assign region lablels
+quotalabs = ['GLM9','GLM1','GLM2','GLM3_east','Chatham Islands', 'GLM3_west','Stewart Island', 'Auckland Islands','GLM7B','GLM7A','GLM8']
+regionlabs = ['waikato', '90milebeach_northland', 'hauraki_northland', 'bay_of_plenty', 'hawkes_bay', 'wellington_wairarapa', 'canterbury', 'chatham islands', 'otago', 'southland', 'stewart island', 'auckland islands', 'fiordland', 'west_coast', 'nelson_marlborough', 'wanganui', 'taranaki']
+df = pd.DataFrame(data = y, index = regionlabs, columns = regionlabs)
+
+#plot matrix
+ax = sns.heatmap(df, mask = (df == 0))
+
+############
+#plot points
+############
+def plot_sfpoints(sfpoints):
+    '''
+    Plot start and final position of each particle.
+
+    sfpoints: a list of lists x where each element of x is a list that contains
+    the start and final locations as shapely Points and end status for each particle.
+    e.g. x = [[Point,Point, status],[Point, Point, status]]
+    '''
+    for x in sfpoints:    
+        for i in x:
+            sx = i[0].x
+            sy = i[0].y
+            fx = i[1].x
+            fy = i[1].y
+            plt.plot(sx, sy, 'go')
+            if i[2] == 'active':
+                plt.plot(fx, fy, 'bo')
+            if i[2] == 'died':
+                plt.plot(fx, fy, 'yo')
+            if i[2] == 'settled_on_coast':
+                plt.plot(fx, fy, 'ro')
+            if i[2] == 'settled_on_bottom':
+                plt.plot(fx, fy, 'mo')
+    m = Basemap(resolution= 'h', llcrnrlon = 162, llcrnrlat = -51, urcrnrlon = 186, urcrnrlat = -32)
+    m.drawcoastlines()
+    plt.show()
+
+
+
+#############
+#just for fun
+#############
 #exclude empty rows
 nonemptyr = 0
-for i in conmatpercent:
+for i in matrix:
     if sum(i) > 0:
         nonemptyr += 1
 
-musmat = np.empty((nonemptyr, len(bins)))
+matrixr = np.empty((nonemptyr, len(bins)))
 
 count = 0
-for i in range(len(conmatpercent)):
-    if sum(conmatpercent[i] > 0):
-        musmat[count] = conmatpercent[i]
+for i in range(len(matrix)):
+    if sum(matrix[i] > 0):
+        matrixr[count] = matrix[i]
         count += 1
 
 #exclude empty columns
 nonemptyc = 0
-for i in range(len(musmat[0,:])):
-    if sum(musmat[:,i]) > 0:
+for i in range(len(matrixr[0,:])):
+    if sum(matrixr[:,i]) > 0:
         nonemptyc +=1
 
-musmattrim = np.empty((nonemptyr,nonemptyc))
+matrixtrim = np.empty((nonemptyr,nonemptyc))
 
 count = 0 
-for i in range(len(musmat[0,:])):
-    if sum(musmat[:,i]) > 0:
-        musmattrim[:, count] = musmat[:,i]
+for i in range(len(matrixr[0,:])):
+    if sum(matrixr[:,i]) > 0:
+        matrixtrim[:, count] = matrixr[:,i]
         count += 1
-
-#create heatmap
-import seaborn as sns
-ax = sns.heatmap(musmattrim, mask = (musmattrim == 0))
-
-
-
-#create conmat based on regions. set np.zeros((11,12)) if using quota areas
-regionconmat = np.zeros((14, 15))
-
-startfinalregions = []
-for i in range(len(finalpoints)):
-    start_bin_region = grid.get_bin_region(startpoints[i].x, startpoints[i].y)
-    final_bin_region = grid.get_bin_region(finalpoints[i].x, finalpoints[i].y)
-    startfinalregions.append([start_bin_region, final_bin_region])
-
-for i in startfinalregions:
-    if i[0] >= 0:
-        if i[1] == -1:
-            regionconmat[i[0], -1] += 1
-        else:
-            regionconmat[i[0], i[1]] += 1
-
-#convert to percent settlers
-regionconmatpercent = np.zeros((14, 15))
-for i in range(10):
-    if sum(regionconmat[i]) > 0:
-        regionconmatpercent[i] = regionconmat[i]/sum(regionconmat[i])
-
-regionconmatpercent = regionconmatpercent[:,:-1]
-
-#assign region names to dataframe
-import pandas as pd
-
-quotalabs = ['GLM9','GLM1','GLM2','GLM3_east','Chatham Islands', 'GLM3_west','Stewart Island', 'Auckland Islands','GLM7B','GLM7A','GLM8']
-regionlabs = ['waikato', '90milebeach_northland', 'hauraki_northland', 'bay_of_plenty', 'hawkes_bay', 'wellington_wairarapa', 'canterbury', 'otago', 'southland', 'fiordland', 'west_coast', 'nelson_marlborough', 'wanganui', 'taranaki']
-df = pd.DataFrame(data = regionconmatpercent, index = regionlabs, columns = regionlabs)
-
-
-#create regional heatmap
-ax = sns.heatmap(df, mask = (df == 0))
-
-
-
 
