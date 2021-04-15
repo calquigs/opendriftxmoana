f1c000dd8421a39da1693bdea4f7d28ed4f9fda7
calquigs <55519685+calquigs@users.noreply.github.com>
2021-04-15 12:17:42 +1200

    added scripts directory

---
+++ scripts/variability_test_reinga_customoutput.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+# 
+import os
+import sys
+import time
+import numpy as np
+import matplotlib
+from datetime import datetime, timedelta
+from opendrift.readers import reader_ROMS_native_MOANA
+from opendrift.models.bivalvelarvae import BivalveLarvae
+
+
+###############################
+# MODEL SELECTION
+###############################
+o = BivalveLarvae(loglevel=0)#,logfile='mussel_forwardtrack_%s_%s.log' % (year,month) )  # Set loglevel to 0 for debug information
+###############################
+# READERS
+###############################
+
+
+data_path0 = '/nesi/nobackup/mocean02574/NZB_N50/nz5km_his_201512.nc'
+data_path1 = '/nesi/nobackup/mocean02574/NZB_N50/nz5km_his_201601.nc'
+data_path2 = '/nesi/nobackup/mocean02574/NZB_N50/nz5km_his_201602.nc'
+data_path3 = '/nesi/nobackup/mocean02574/NZB_N50/nz5km_his_201603.nc'
+data_path4 = '/nesi/nobackup/mocean02574/NZB_N50/nz5km_his_201604.nc'
+data_path5 = '/nesi/nobackup/mocean02574/NZB_N50/nz5km_his_201605.nc'
+data_path6 = '/nesi/nobackup/mocean02574/NZB_N50/nz5km_his_201606.nc'
+#data_path = '/nesi/project/mocean02574/output/moana_backbone/nz5km_his_201712.nc'
+#data_path = '/ocean/Moana/NZB/moana_backboneV2/his/'
+#data_path = 'http://thredds.moanaproject.org:8080/thredds/dodsC/moana/ocean/NZB/v1.9/raw_3D/nz5km_his_201712.nc'
+
+# reader_moana_dec15 = reader_ROMS_native_MOANA.Reader(data_path+"nz5km_his_201707.nc") # load data for that year
+# reader_moana_dec15.multiprocessing_fail = True # thisb ypasses the use of multi core for coordinates conversion and seems to make the model run much faster.
+
+
+reader_moana_dec15 = reader_ROMS_native_MOANA.Reader(data_path0) # load data for that year
+reader_moana_jan16 = reader_ROMS_native_MOANA.Reader(data_path1) # load data for that year
+reader_moana_feb16 = reader_ROMS_native_MOANA.Reader(data_path2) # load data for that year
+reader_moana_mar16 = reader_ROMS_native_MOANA.Reader(data_path3) # load data for that year
+reader_moana_apr16 = reader_ROMS_native_MOANA.Reader(data_path4) # load data for that year
+reader_moana_may16 = reader_ROMS_native_MOANA.Reader(data_path5) # load data for that year
+reader_moana_jun16 = reader_ROMS_native_MOANA.Reader(data_path6) # load data for that year
+reader_moana_dec15.multiprocessing_fail = True # bypass the use of multi core for coordinates conversion and seems to make the model run much faster.
+reader_moana_jan16.multiprocessing_fail = True # bypass the use of multi core for coordinates conversion and seems to make the model run much faster.
+reader_moana_feb16.multiprocessing_fail = True # bypass the use of multi core for coordinates conversion and seems to make the model run much faster.
+reader_moana_mar16.multiprocessing_fail = True # bypass the use of multi core for coordinates conversion and seems to make the model run much faster.
+reader_moana_apr16.multiprocessing_fail = True # bypass the use of multi core for coordinates conversion and seems to make the model run much faster.
+reader_moana_may16.multiprocessing_fail = True # bypass the use of multi core for coordinates conversion and seems to make the model run much faster.
+reader_moana_jun16.multiprocessing_fail = True # bypass the use of multi core for coordinates conversion and seems to make the model run much faster.
+
+# # Making customised landmask - not required here, we are using the ROMS landmask i.e. included in netcdf files
+# reader_landmask = reader_global_landmask.Reader(
+#                     llcrnrlon=171.0, llcrnrlat=184.5,
+#                     urcrnrlon=-42.0, urcrnrlat=-34.0)
+
+# use native landmask of ROMS files
+o.add_reader([reader_moana_dec15, reader_moana_jan16, reader_moana_feb16, reader_moana_mar16, reader_moana_apr16, reader_moana_mar16, reader_moana_jun16]) # 
+o.set_config('general:use_auto_landmask', False) # prevent opendrift from making a new dynamical landmask
+
+###############################
+# PARTICLE SEEDING
+###############################
+#Reinga
+lons = [172.75, 172.8, 172.8, 172.75]
+lats = [-34.3, -34.3, -34.35, -34.35]
+
+def create_seed_times(start, end, delta):
+  """
+  crate times at given interval to seed particles
+  """
+  out = []
+  start_t = start
+  end_t = datetime.strptime(str(end), "%Y-%m-%d %H:%M:%S")
+  while start_t < end:
+    out.append(start_t) 
+    start_t += delta
+  return out
+
+times = create_seed_times(reader_moana_dec15.start_time, 
+                          reader_moana_apr16.end_time, timedelta(hours = 2))
+
+
+number = 100
+z = np.random.uniform(-10,0,size=len(times)) # generate random depth
+
+for i in range(len(times)):
+  o.seed_within_polygon(lons, lats, number = number, time = times[i], z = z[i])
+
+###############################
+# PHYSICS
+###############################
+# these will list all possible options for that model
+o.set_config('environment:fallback:x_wind', 0.0)
+o.set_config('environment:fallback:y_wind', 0.0)
+o.set_config('environment:fallback:x_sea_water_velocity', 0.0)
+o.set_config('environment:fallback:y_sea_water_velocity', 0.0)
+o.set_config('environment:fallback:sea_floor_depth_below_sea_level', 100000.0)
+
+# No diffusion - constant in that example
+#Kxy = 1.0 #0.1176 # m2/s-1
+#Kz = 0.001 # m2/s-1
+Kxy = 0.1176  #0.1176 # m2/s-1
+Kz = 0.01# m2/s-1
+o.set_config('drift:horizontal_diffusivity',Kxy) # using new config rather than current uncertainty
+o.set_config('environment:fallback:ocean_vertical_diffusivity', Kz) # specify constant ocean_vertical_diffusivity in m2.s-1
+# seed
+o.set_config('seed:ocean_only',True) # keep only particles from the "frame" that are on the ocean
+# drift
+o.set_config('drift:advection_scheme','runge-kutta4') # or 'runge-kutta'
+o.set_config('drift:current_uncertainty', 0.0 ) # note current_uncertainty can be used to replicate an horizontal diffusion spd_uncertain = sqrt(Kxy*2/dt)  
+o.set_config('drift:max_age_seconds', 3600*24*30) # 
+o.set_config('drift:lift_to_seafloor',True)
+#processes
+o.set_config('drift:vertical_mixing', False)  
+o.set_config('general:coastline_action','previous') # option('none', 'stranding', 'previous', default='stranding')
+#o.set_config('drift:min_settlement_age_seconds', 3600*24*21) #
+
+o.list_config()
+# o.list_configspec()
+
+###############################
+# RUN 
+###############################
+# Running model (until end of driver data)
+
+lons_start = o.elements_scheduled.lon
+lats_start = o.elements_scheduled.lat
+
+o.run(stop_on_error=False,
+      end_time=reader_moana_jun16.end_time,
+      time_step=900, 
+      time_step_output = 86400.0,
+      export_variables = [])
+
+index_of_first, index_of_last = o.index_of_activation_and_deactivation()
+lons = o.get_property('lon')[0]
+lats = o.get_property('lat')[0]
+status = o.get_property('status')[0]
+lons_end = lons[index_of_last, range(lons.shape[1])]
+lats_end = lats[index_of_last, range(lons.shape[1])]
+status_end = status[index_of_last, range(lons.shape[1])]
+
+
+outFile = open('variability_test_reinga.txt','w')
+
+for i in range(len(lons_end)):
+  outFile.write(lons_start[i]+","+lats_start[i]+","+lons_end[i]+","+lats_end[i]+","+status_end[i]+"\n")
+
+
+
+#o.plot(filename = "nparticles_test.jpg")
+
+#o.animation(filename = "14mussels_2015.mp4")
+
+
+
